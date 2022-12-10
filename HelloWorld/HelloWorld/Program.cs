faadc11366dc171b34e649f25b900357a9d1ef60
Evan Green <evan@cgmicrotech.com>
2022-12-10 17:59:42 -0600

    line addtion fix

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,19 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 Main.ShowMessage();
 
 
 public class Main
 {
-    private string evan = "Evan";
+    private string evan = "Evan Green";
     private string dad = "Dad";
 
     public static void ShowMessage()
     {
         var main = new Main();
 
-        Console.WriteLine($"Hello {main.evan} and {main.daddy}");
-        Console.WriteLine("Hello my dad");
+        Console.WriteLine($"Hello {main.evan} and {main.dad}");
     }
 }
 
